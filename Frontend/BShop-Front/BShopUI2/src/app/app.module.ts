8048eeff5df120767f721515128fa78f63617dee
X.V <academic122@gmail.com>
2021-03-18 09:48:40 +0330

    creating new UI

---
+++ Frontend/BShop-Front/BShopUI2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemPageComponent } from './item-page/item-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
